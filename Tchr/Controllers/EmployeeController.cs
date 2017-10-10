5494ff5a0bc77624b1f4f3a3bdc602cb62ad1398
Satheesh Kumar Ramasamy <satheesh@thoughtclan.com>
2017-10-10 17:39:30 +0530

    Added employee controller

---
+++ Tchr/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Http;
+using Tchr.Models;
+
+namespace Tchr.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IHttpActionResult GetEmployee(int id)
+        {
+            var employee = new Employee
+            {
+                Id = 1,
+                FirstName = "Satheesh",
+                LastName = "Ramasamy"
+            };
+            return Ok(employee);
+        }
+    }
+}
